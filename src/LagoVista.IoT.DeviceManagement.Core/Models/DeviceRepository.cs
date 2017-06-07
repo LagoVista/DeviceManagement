4a9b35703df7489b972399133358b49694ca53a0
Kevin D. Wolf <kevinw@software-logistics.com>
2017-06-07 16:47:11 -0400

    Full Commit

--- src/LagoVista.IoT.DeviceManagement.Core/Models/DeviceRepository.cs
+++ src/LagoVista.IoT.DeviceManagement.Core/Models/DeviceRepository.cs
@@ -1,10 +1,49 @@
-﻿using System;
+﻿using LagoVista.Core.Attributes;
+using LagoVista.Core.Interfaces;
+using LagoVista.Core.Models;
+using LagoVista.Core.Validation;
+using LagoVista.IoT.DeviceManagement.Core.Resources;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace LagoVista.IoT.DeviceManagement.Core.Models
 {
-    class DeviceRepository
+    [EntityDescription(DeviceManagementDomain.DeviceManagement, DeviceManagementResources.Names.DeviceGroup_Title, Resources.DeviceManagementResources.Names.DeviceGroup_Help, Resources.DeviceManagementResources.Names.DeviceGroup_Description, EntityDescriptionAttribute.EntityTypes.SimpleModel, typeof(DeviceManagementResources))]
+    public class DeviceRepository : LagoVista.IoT.DeviceAdmin.Models.IoTModelBase, IOwnedEntity, IKeyedEntity, INoSQLEntity, IValidateable, IEntityHeaderEntity
     {
+        public string DatabaseName { get; set; }
+        public string EntityType { get; set; }
+
+        public bool IsPublic { get; set; }
+        public EntityHeader OwnerOrganization { get; set; }
+        public EntityHeader OwnerUser { get; set; }
+
+        public IConnectionSettings DeviceStorageSettings { get; set; }
+
+
+        [FormField(LabelResource: Resources.DeviceManagementResources.Names.Common_Key, HelpResource: Resources.DeviceManagementResources.Names.Common_Key_Help, FieldType: FieldTypes.Key, RegExValidationMessageResource: Resources.DeviceManagementResources.Names.Common_Key_Validation, ResourceType: typeof(DeviceManagementResources), IsRequired: true)]
+        public string Key { get; set; }
+
+        public DeviceGroupSummary CreateSummary()
+        {
+            return new DeviceGroupSummary()
+            {
+                Id = Id,
+                Description = Description,
+                IsPublic = IsPublic,
+                Key = Key,
+                Name = Name
+            };
+        }        
+
+        public IEntityHeader ToEntityHeader()
+        {
+            return new EntityHeader()
+            {
+                Id = Id,
+                Text = Name,         
+            };
+        }
     }
 }
