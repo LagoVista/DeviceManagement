f6f2bdf5d4cfce08cc7380bcec4443a7e4a245e4
Kevin D Wolf <kevin@DESKTOP-R4064KP.(none)>
2017-06-30 07:23:40 -0400

    Cloud Storage Update/Add default input translator

--- src/LagoVista.IoT.DeviceManagement.Core/Models/DeviceArchives.cs
+++ src/LagoVista.IoT.DeviceManagement.Core/Models/DeviceArchives.cs
@@ -11,35 +11,24 @@ namespace LagoVista.IoT.DeviceManagement.Core.Models
     {
         public DeviceArchive()
         {
-            Properties = new List<CustomField>();
+            Properties = new Dictionary<string, object>();
         }
-
-        /* Partition Key will be the give id */
 
         /* Device ID is the ID associated with the device by the user, it generally will be unique, but can't assume it to be, it's primarily read only */
         public string DeviceId { get; set; }
 
         public string DeviceConfigurationId { get; set; }
 
-        public string DateStamp { get; set; }
+        public string Timestamp { get; set; }
 
         public string PEMMessageId { get; set; }
 
         /* Will be made available to map to properties on device configurations */
         public double DeviceConfigurationVersionId { get; set; }
+ 
 
-        /* Will be smart and dynamically add properties that get serialized with Table Storage */
-        /* Will then be smart and construct a deserialized json package with the properties appropriate for this device */
-
-
-        public String PropertiesJSON
-        {
-            get { return JsonConvert.SerializeObject(Properties);  }
-            set { Properties = JsonConvert.DeserializeObject<List<CustomField>>(value); }
-        }
-
-        [JsonIgnore]
-        public List<CustomField> Properties { get; set; }
+        [JsonExtensionData]
+        public Dictionary<String, object> Properties { get; set; }
 
     }
 }
