355309db407d44d362655b68299be3261282d775
Kevin D Wolf <kevin@DESKTOP-R4064KP.(none)>
2017-09-28 09:12:37 -0400

    start adding IoT Hub Support

---
+++ src/LagoVista.IoT.DeviceManagement.Repos/DTOs/AzureIoTDevice.cs
@@ -0,0 +1,113 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.IoT.DeviceManagement.Repos.DTOs
+{
+    /*
+    https://docs.microsoft.com/en-us/rest/api/iot-dps/deviceenrollment/createorupdate#definitions_attestationmechanism
+    */
+    public class AzureIoTDevice
+    {
+
+    }
+
+    public class SymmetericKey
+    {
+        [JsonProperty("primaryKey")]
+        public string PrimaryKey { get; set; }
+
+        [JsonProperty("secondaryKey")]
+        public string SecondaryKey { get; set; }
+    }
+
+    public class AttestationMechanism
+    {
+        public TpmAttestation tpm { get; set; }
+
+        public string type { get; set; }
+        public X509Attestation x509 { get; set; }
+    }
+
+    public class DeviceRegistrationStatus
+    {
+        public string assignedHub { get; set; }
+        public string createdDateTimeUtc { get; set; }
+        public string deviceId { get; set; }
+        public Int32 errorCode { get; set; }
+        public string errorMessage { get; set; }
+        public string etag { get; set; }
+        public string generationId { get; set; }
+        public string lastUpdatedDateTimeUtc { get; set; }
+        public string registrationId { get; set; }
+        public string status { get; set; }
+    }
+
+    public class Enrollment
+    {
+        public AttestationMechanism attestation { get; set; }
+
+        public string createdDateTimeUtc { get; set; }
+        public string deviceId { get; set; }
+        public string etag { get; set; }
+        public string generationId { get; set; }
+        public TwinState initialTwinState { get; set; }
+        public string iotHubHostName { get; set; }
+        public string lastUpdatedDateTimeUtc { get; set; }
+        public string provisioningStatus { get; set; }
+        public string registrationId { get; set; }
+
+        public DeviceRegistrationStatus registrationStatus { get; set; }
+    }
+
+    public class TpmAttestation
+    {
+        public string endorsementKey { get; set; }
+        public string storageRootKey { get; set; }
+    }
+
+    public class TwinState
+    {
+        [JsonProperty("desiredProperties")]
+        public Object[] DesiredProperties { get; set; }
+
+
+        [JsonProperty("tags")]
+        public Object[] Tags { get; set; }
+    }
+
+    public class X509Attestation
+    {
+        public X509Certificates clientCertificates { get; set; }
+        public X509Certificates signingCertificates { get; set; }
+    }
+
+    public class X509Certificates
+    {
+        public X509CertificateWithInfo primary { get; set; }
+        public X509CertificateWithInfo secondary { get; set; }
+
+    }
+
+    public class X509CertificateWithInfo
+    {
+        public string kcertificate { get; set; }
+
+        public X509CertificateInfo info { get; set; }
+    }
+
+    public class X509CertificateInfo
+    {
+        public string issuerName { get; set; }
+
+        public string notAfterUtc { get; set; }
+        public string notBeforeUtc { get; set; }
+        public string serialNumber { get; set; }
+        public string sha1Thumbprint { get; set; }
+        public string sha256Thumbprint { get; set; }
+        public string subjectName { get; set; }
+        public Int32 version { get; set; }
+    }
+    
+}
---
+++ src/LagoVista.IoT.DeviceManagement.Repos/Repos/IoTHubDeviceManagementRepo.cs
@@ -0,0 +1,85 @@
+﻿using LagoVista.IoT.DeviceManagement.Core.Repos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using LagoVista.IoT.DeviceManagement.Core.Models;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace LagoVista.IoT.DeviceManagement.Repos.Repos
+{
+    public class IoTHubDeviceManagementRepo : IDeviceManagementRepo
+    {
+
+        public IoTHubDeviceManagementRepo()
+        {
+
+        }
+
+        public Task AddDeviceAsync(DeviceRepository repo, Device device)
+        {
+            var deviceIdRegEx = new Regex(@"^[A-Za-z0-9\-:.+%_#*?!(),=@;$']{1,128}$");
+
+            var apiVersion = "2016-11-14";
+
+            var uri = $"https://{repo.Key}.bytemaster.azure-devices.net/devices/{device.DeviceId}?api-version={apiVersion}";
+
+            return Task.FromResult<object>(default(object));
+        }
+
+        public Task<bool> CheckIfDeviceIdInUse(DeviceRepository repo, string id, string orgid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteDeviceAsync(DeviceRepository repo, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteDeviceByIdAsync(DeviceRepository repo, string deviceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Device> GetDeviceByDeviceIdAsync(DeviceRepository repo, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Device> GetDeviceByIdAsync(DeviceRepository repo, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DeviceSummary>> GetDevicesForLocationIdAsync(DeviceRepository repo, string locationId, int top, int take)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DeviceSummary>> GetDevicesForOrgIdAsync(DeviceRepository repo, string orgId, int top, int take)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DeviceSummary>> GetDevicesInStatusAsync(DeviceRepository repo, string status, int top, int take)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DeviceSummary>> GetDevicesWithConfigurationAsync(DeviceRepository repo, string configurationId, int top, int take)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DeviceSummary>> GetDevicesWithDeviceTypeAsync(DeviceRepository repo, string deviceTypeId, int top, int take)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateDeviceAsync(DeviceRepository repo, Device device)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
