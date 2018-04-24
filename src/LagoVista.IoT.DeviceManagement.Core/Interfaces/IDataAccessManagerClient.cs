4aeb8e795d81fb9b4cf66e4a7e1790bc6fd3af29
Kevin D. Wolf <kevinw@software-logistics.com>
2018-04-24 09:06:22 -0400

    Add DAM projects to device manager

---
+++ src/LagoVista.IoT.DeviceManagement.Core/Interfaces/IDataAccessManagerClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.IoT.DeviceManagement.Core.Interfaces
+{
+    interface IDataAccessManagerClient
+    {
+    }
+}
