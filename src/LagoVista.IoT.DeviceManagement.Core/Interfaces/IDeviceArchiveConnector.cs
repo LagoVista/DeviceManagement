2f7cc690bc018432337cd54fe031dcbf8affa0bc
Mark Lauter <mark.lauter@sumosoftware.com>
2018-05-23 20:17:49 -0400

    recovered I...Connector interfaces

---
+++ src/LagoVista.IoT.DeviceManagement.Core/Interfaces/IDeviceArchiveConnector.cs
@@ -0,0 +1,16 @@
+﻿using LagoVista.Core.Models;
+using LagoVista.Core.Models.UIMetaData;
+using LagoVista.IoT.DeviceManagement.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagoVista.IoT.DeviceManagement.Core.Interfaces
+{
+    public interface IDeviceArchiveConnector
+    {
+        Task AddArchiveAsync(string instanceId, DeviceArchive archiveEntry, EntityHeader org, EntityHeader user);
+        Task<ListResponse<List<object>>> GetForDateRangeAsync(string instanceId, string deviceId, ListRequest request, EntityHeader org, EntityHeader user);
+    }
+}
---
+++ src/LagoVista.IoT.DeviceManagement.Core/Interfaces/IDeviceManagementConnector.cs
@@ -0,0 +1,44 @@
+﻿using LagoVista.Core.Models;
+using LagoVista.Core.Models.UIMetaData;
+using LagoVista.Core.Validation;
+using LagoVista.IoT.DeviceManagement.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagoVista.IoT.DeviceManagement.Core.Interfaces
+{
+    public interface IDeviceManagementConnector
+    {
+        Task<InvokeResult> AddDeviceAsync(string instanceId, Device device, EntityHeader org, EntityHeader user);
+
+        Task DeleteDeviceAsync(string instanceId, string id, EntityHeader org, EntityHeader user);
+
+        Task UpdateDeviceAsync(string instanceId, Device device, EntityHeader org, EntityHeader user);
+
+        Task<ListResponse<DeviceSummary>> GetDevicesForOrgIdAsync(string instanceId, ListRequest listRequest, EntityHeader org, EntityHeader user);
+
+        Task<ListResponse<DeviceSummary>> GetDevicesForLocationIdAsync(string instanceId, string locationId, ListRequest listRequest, EntityHeader org, EntityHeader user);
+
+        Task<Device> GetDeviceByDeviceIdAsync(string instanceId, string id, EntityHeader org, EntityHeader user);
+
+        Task<bool> CheckIfDeviceIdInUse(string instanceId, string id, EntityHeader org, EntityHeader user);
+
+        Task<Device> GetDeviceByIdAsync(string instanceId, string id, EntityHeader org, EntityHeader user);
+
+        Task<ListResponse<DeviceSummary>> GetDevicesInStatusAsync(string instanceId, string status, ListRequest listRequest, EntityHeader org, EntityHeader user);
+
+        Task<ListResponse<DeviceSummary>> GetDevicesInCustomStatusAsync(string instanceId, string status, ListRequest listRequest, EntityHeader org, EntityHeader user);
+
+        Task<ListResponse<DeviceSummary>> GetDevicesWithConfigurationAsync(string instanceId, string configurationId, ListRequest listRequest, EntityHeader org, EntityHeader user);
+
+        Task<ListResponse<Device>> GetFullDevicesWithConfigurationAsync(string instanceId, string configurationId, ListRequest listRequest, EntityHeader org, EntityHeader user);
+
+        Task<ListResponse<DeviceSummary>> GetDevicesWithDeviceTypeAsync(string instanceId, string deviceTypeId, ListRequest listRequest, EntityHeader org, EntityHeader user);
+
+        Task<ListResponse<DeviceSummary>> SearchByDeviceIdAsync(string instanceId, string search, ListRequest listRequest, EntityHeader org, EntityHeader user);
+        
+        Task<ListResponse<DeviceSummaryData>> GetDeviceGroupSummaryDataAsync(string instanceId, string groupId, ListRequest listRequest, EntityHeader org, EntityHeader user);
+    }
+}
