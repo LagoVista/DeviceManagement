283b94c9ad2d3f1a3bc991e34b8829716ebbf0d9
Kevin D Wolf <kevin@DESKTOP-R4064KP.(none)>
2018-06-02 14:17:27 -0400

    fix bug in PEM restore test

--- src/LagoVista.IoT.DeviceManagement.Repos/Repos/PEMIndex.cs
+++ src/LagoVista.IoT.DeviceManagement.Repos/Repos/PEMIndex.cs
@@ -58,6 +58,7 @@ namespace LagoVista.IoT.DeviceManagement.Repos.Repos
                     var log = JsonConvert.DeserializeObject(Log) as JArray;
                     if (log != null)
                     {
+                        if (pem.ContainsKey("log")) pem.Remove("log");
                         pem.Add("log", Log);
                     }
                 }
@@ -67,6 +68,7 @@ namespace LagoVista.IoT.DeviceManagement.Repos.Repos
                     var device = JsonConvert.DeserializeObject(Device) as JObject;
                     if (device != null)
                     {
+                        if (pem.ContainsKey("device")) pem.Remove("device");
                         pem.Add("device", device);
                     }
                 }
@@ -76,6 +78,7 @@ namespace LagoVista.IoT.DeviceManagement.Repos.Repos
                     var instructions = JsonConvert.DeserializeObject(Instructions) as JArray;
                     if (instructions != null)
                     {
+                        if (pem.ContainsKey("instructions")) pem.Remove("instructions");
                         pem.Add("instructions", instructions);
                     }
                 }
@@ -85,6 +88,7 @@ namespace LagoVista.IoT.DeviceManagement.Repos.Repos
                     var responseMessage = JsonConvert.DeserializeObject(ResponseMessage) as JObject;
                     if (responseMessage != null)
                     {
+                        if (pem.ContainsKey("responseMessage")) pem.Remove("responseMessage");
                         pem.Add("responseMessage", responseMessage);
                     }
                 }
@@ -94,6 +98,7 @@ namespace LagoVista.IoT.DeviceManagement.Repos.Repos
                     var outgoingMessages = JsonConvert.DeserializeObject(OutgoingMessages) as JArray;
                     if (outgoingMessages != null)
                     {
+                        if (pem.ContainsKey("outgoingMessages")) pem.Remove("outgoingMessages");
                         pem.Add("outgoingMessages", outgoingMessages);
                     }
                 }
