08d017ead924a4314a4b3c283b606ed686c44507
Kevin D. Wolf <kevinw@software-logistics.com>
2024-07-06 08:06:08 -0400

    remote config mgmt for device commands

--- src/LagoVista.IoT.DeviceManagement.Core/Interfaces/IRemoteConfigurationManager.cs
+++ src/LagoVista.IoT.DeviceManagement.Core/Interfaces/IRemoteConfigurationManager.cs
@@ -1,5 +1,6 @@
 ﻿using LagoVista.Core.Models;
 using LagoVista.Core.Validation;
+using LagoVista.IoT.DeviceAdmin.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -14,5 +15,7 @@ namespace LagoVista.IoT.DeviceManagement.Core
         Task<InvokeResult> QueryRemoteConfigurationAsync(string deviceRepoId, string deviceUniqueId, EntityHeader org, EntityHeader user);
         Task<InvokeResult> RestartDeviceAsync(string deviceRepoId, string deviceUniqueId, EntityHeader org, EntityHeader user);
         Task<InvokeResult<string>> ApplyFirmwareAsync(string deviceRepoId, string deviceUniqueId, string firmwareId, string firmwareRevisionId, bool triggerOnDevice, EntityHeader org, EntityHeader user);
+        Task<InvokeResult> SendCommandAsync(string deviceRepoId, string deviceUniqueId, string commandId, List<KeyValuePair<string, string>> parameters, EntityHeader org, EntityHeader user);
+       
     }
 }
--- src/LagoVista.IoT.DeviceManagement.Core/Interfaces/IRemotePropertyNamanager.cs
+++ src/LagoVista.IoT.DeviceManagement.Core/Interfaces/IRemotePropertyNamanager.cs
@@ -1,4 +1,6 @@
 ﻿using LagoVista.Core.Validation;
+using LagoVista.IoT.DeviceAdmin.Models;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace LagoVista.IoT.DeviceManagement.Core.Interfaces
@@ -6,6 +8,7 @@ namespace LagoVista.IoT.DeviceManagement.Core.Interfaces
     public interface IRemotePropertyNamanager
     {
         Task<InvokeResult> SendPropertyAsync(string deviceUniqueId, int propertyIndex);
+        Task<InvokeResult> SendCommandAsync(string deviceUniqueId, string command, List<KeyValuePair<string, string>> parameters);
         Task<InvokeResult> SendAllPropertiesAsync(string deviceUniqueId);
         Task<InvokeResult> QueryRemoteConfigurationAsync(string deviceUniqueId);
         Task<InvokeResult> RestartDeviceAsync(string deviceUniqueId);
