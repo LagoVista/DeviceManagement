2fc3477077ff0a9d29cef8520ad6e5c59f806246
Kevin D. Wolf <kevinw@software-logistics.com>
2026-07-27 10:07:20 -0400

    remove old assembly info for compile

--- src/LagoVista.IoT.DeviceManagement.Models/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-// --- BEGIN CODE INDEX META (do not edit) ---
-// ContentHash: 37570232a126ab7a4b82f2f7b58d585c03bcdb6b5575a2a81656df59ec554735
-// IndexVersion: 2
-// --- END CODE INDEX META ---
-using System.Resources;
-
-[assembly: NeutralResourcesLanguage("en")]
